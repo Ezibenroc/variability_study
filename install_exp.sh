2b1d7045103919a6871f77563949a6c51bc93868
Tom Cornebize <tom.cornebize@ensimag.grenoble-inp.fr>
2017-10-31 09:32:36 -0500

    Update install script.

--- install_exp.sh
+++ install_exp.sh
@@ -28,7 +28,7 @@ for host in $*; do {
     run_command ${host} 'wget https://bootstrap.pypa.io/get-pip.py && python3 get-pip.py && pip3 install psutil'
     run_command ${host} 'cd scripts/cblas_tests/intercoolr && make'
     run_command ${host} 'cd scripts/cblas_tests && python3 ./runner.py --csv_file /tmp/test.csv --lib openblas --dgemm -s 64,64 -n 1 -r 1 --stat'
-    run_command ${host} 'cd scripts/cblas_tests && python3 ./multi_runner.py --nb_runs 10 --nb_calls 10 --size 100 -np 8 --csv_file /tmp/test.csv --lib openblas'
+    run_command ${host} 'cd scripts/cblas_tests && python3 ./multi_runner.py --nb_runs 10 --nb_calls 10 --size 100 -np 8 --csv_file /tmp/test.csv --lib naive'
     echo "DONE for ${host}"
 }&
 done
