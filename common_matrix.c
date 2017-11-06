d85d43a8e16a6bff2d88c0e527b38f90f45441ce
Tom Cornebize <tom.cornebize@ensimag.grenoble-inp.fr>
2017-11-06 10:50:00 -0600

    Do the naive matrix product per block (duration is 3.5 times shorter for the 4,096 size).

--- common_matrix.c
+++ common_matrix.c
@@ -17,18 +17,30 @@
 #include <cblas.h>
 #endif
 
+#define BLOCK_SIZE 128
+
+inline int min(int a, int b) {
+    return a < b ? a : b;
+}
+
 void matrix_product(double *A, double *B, double *C, int size) {
 #ifdef USE_NAIVE
     #pragma omp parallel
     {
-    for(int k = 0 ; k < size ; k++) {
+    for(int i0 = 0 ; i0 < size ; i0 += BLOCK_SIZE) {
         #pragma omp for // collapse(2) // ← doing the j-loop in parallel slow down the execution by a factor 2 (hypothesis: more cache miss)
-        for(int i = 0 ; i < size ; i++) {
-            for(int j = 0 ; j < size ; j++) {
-                double a = matrix_get(A, size, i, k);
-                double b = matrix_get(B, size, k, j);
-                double c = matrix_get(C, size, i, j);
-                matrix_set(C, size, i, j, c + a*b);
+        for(int j0 = 0 ; j0 < size ; j0 += BLOCK_SIZE) {
+            for(int k0 = 0 ; k0 < size ; k0 += BLOCK_SIZE) {
+                for(int k = k0 ; k < min(k0+BLOCK_SIZE, size) ; k++) {
+                    for(int i = i0 ; i < min(i0+BLOCK_SIZE, size) ; i++) {
+                        for(int j = j0 ; j < min(j0+BLOCK_SIZE, size) ; j++) {
+                            double a = matrix_get(A, size, i, k);
+                            double b = matrix_get(B, size, k, j);
+                            double c = matrix_get(C, size, i, j);
+                            matrix_set(C, size, i, j, c + a*b);
+                        }
+                    }
+                }
             }
         }
     }
