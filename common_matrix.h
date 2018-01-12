aad98d54c809d973c1ab668ec1df8cd48a5cb5f1
Tom Cornebize <tom.cornebize@univ-grenoble-alpes.fr>
2018-01-12 14:49:01 +0100

    Fix common_matrix.h (inline → static inline).

--- common_matrix.h
+++ common_matrix.h
@@ -4,22 +4,22 @@
 #include <string.h>
 #include <stdlib.h>
 
-inline double *allocate_matrix(int size) {
+static inline double *allocate_matrix(int size) {
     double *result = (double*) malloc(size*size*sizeof(double));
     memset(result, 1, size*size*sizeof(double));
     assert(result);
     return result;
 }
 
-inline void free_matrix(double *matrix) {
+static inline void free_matrix(double *matrix) {
     free(matrix);
 }
 
-inline void matrix_set(double *matrix, int size, int i, int j, double value) {
+static inline void matrix_set(double *matrix, int size, int i, int j, double value) {
     matrix[i*size+j] = value;
 }
 
-inline double matrix_get(double *matrix, int size, int i, int j) {
+static inline double matrix_get(double *matrix, int size, int i, int j) {
     return matrix[i*size+j];
 }
 
