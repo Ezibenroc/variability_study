32f6b95e12be8ae8c8f28347cb702cd2e95f381f
Tom Cornebize <tom.cornebize@univ-grenoble-alpes.fr>
2018-01-12 14:52:29 +0100

    More generic compilation for OpenBLAS.

--- utils.py
+++ utils.py
@@ -43,7 +43,7 @@ def compile_generic(exec_filename, lib, block_size=128, likwid=None):
         'mkl2': ['/opt/intel/bin/icc', '-DUSE_MKL', '-std=gnu99', c_filename, 'common_matrix.c', '-I', '/opt/intel/compilers_and_libraries_2017.0.098/linux/mkl/include',
 		'/opt/intel/mkl/lib/intel64/libmkl_rt.so', '-O3', '-o', exec_filename, *options], # an ugly command for a non-standard library location
         'atlas': ['gcc', '-DUSE_ATLAS', c_filename, '-std=gnu99', 'common_matrix.c', '-fopenmp', '/usr/lib/atlas-base/libcblas.so.3', '-O3', '-o', exec_filename, *options],
-        'openblas': ['gcc', '-DUSE_OPENBLAS', c_filename, '-std=gnu99', 'common_matrix.c', '-fopenmp', '/usr/lib/openblas-base/libblas.so', '-O3', '-o', exec_filename, *options],
+        'openblas': ['gcc', '-DUSE_OPENBLAS', c_filename, '-std=gnu99', 'common_matrix.c', '-fopenmp', '-lopenblas', '-O3', '-o', exec_filename, *options],
         'naive': ['gcc', '-DBLOCK_SIZE=%d' % block_size, *options, '-std=gnu99', '-fopenmp', '-DUSE_NAIVE', c_filename, 'common_matrix.c', '-O3', '-o', exec_filename, *options],
     }
     try:
