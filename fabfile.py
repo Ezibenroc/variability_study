1d5cdf609c558382abb16b9a87549e51197888a3
Tom Cornebize <tom.cornebize@univ-grenoble-alpes.fr>
2018-01-17 11:02:46 +0100

    Revert "Switch to Fabric 2.0."
    This reverts commit dc0b9d7e9b1c2646f873acda363ce3388e4743c8.

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,9 @@
-from invoke import task
+from fabric.api import local, run, cd, env, put
+from fabric.network import ssh
 import os
+
+env.use_ssh_config = True
+ssh.util.log_to_file('/tmp/paramiko.log', 10)
 
 OPENBLAS_ARCHIVE   = 'openblas.zip'
 OPENBLAS_DIRECTORY = 'OpenBLAS-0.2.20'
@@ -32,64 +36,54 @@ PIP_PACKAGES = [
         'lxml',
     ]
 
-@task
-def get_openblas_archive(ctx):
+def get_openblas_archive():
     if not os.path.isfile(OPENBLAS_ARCHIVE):
-        ctx.local('wget %s -O %s' % (OPENBLAS_URL, OPENBLAS_ARCHIVE))
+        local('wget %s -O %s' % (OPENBLAS_URL, OPENBLAS_ARCHIVE))
 
-@task
-def clean(ctx):
-    ctx.run('rm -rf ~/* /usr/lib/openblas-base /usr/lib/libopenblas.so')
-    ctx.run('yes | pip3 uninstall %s' % ' '.join(PIP_PACKAGES))
-    ctx.run('yes | apt remove %s' % ' '.join(APT_PACKAGES))
+def clean():
+    run('rm -rf ~/* /usr/lib/openblas-base /usr/lib/libopenblas.so')
+    run('yes | pip3 uninstall %s' % ' '.join(PIP_PACKAGES))
+    run('yes | apt remove %s' % ' '.join(APT_PACKAGES))
 
-@task
-def copy_archives(ctx):
-    ctx.put('openblas.zip', 'openblas.zip')
-    ctx.put(EXP_ARCHIVE, EXP_ARCHIVE)
+def copy_archives():
+    put('openblas.zip', '~')
+    put(EXP_ARCHIVE, '~')
 
-@task
-def install_apt_packages(ctx):
-    ctx.run('yes | apt upgrade')
-    ctx.run('yes | apt install %s' % ' '.join(APT_PACKAGES))
+def install_apt_packages():
+    run('yes | apt upgrade')
+    run('yes | apt install %s' % ' '.join(APT_PACKAGES))
 
-@task
-def install_openblas(ctx):
-    ctx.run('unzip %s' % OPENBLAS_ARCHIVE)
-    with ctx.cd(OPENBLAS_DIRECTORY):
-        ctx.run('make -j 8')
-        ctx.run('make install PREFIX=/usr')
-    ctx.run('mkdir /usr/lib/openblas-base/')
-    ctx.run('ln -s /usr/lib/libopenblas.so /usr/lib/openblas-base/libblas.so')
+def install_openblas():
+    run('unzip %s' % OPENBLAS_ARCHIVE)
+    with cd(OPENBLAS_DIRECTORY):
+        run('make -j 8')
+        run('make install PREFIX=/usr')
+    run('mkdir /usr/lib/openblas-base/')
+    run('ln -s /usr/lib/libopenblas.so /usr/lib/openblas-base/libblas.so')
 
-@task
-def install_pip_packages(ctx):
-    ctx.run('wget https://bootstrap.pypa.io/get-pip.py')
-    ctx.run('python3 get-pip.py')
-    ctx.run('pip3 install %s' % ' '.join(PIP_PACKAGES))
+def install_pip_packages():
+    run('wget https://bootstrap.pypa.io/get-pip.py')
+    run('python3 get-pip.py')
+    run('pip3 install %s' % ' '.join(PIP_PACKAGES))
 
-@task
-def setup_os(ctx):
-    ctx.run('modprobe msr')
+def setup_os():
+    run('modprobe msr')
 
-@task
-def extract_experiment(ctx):
-    ctx.run('unzip %s' % EXP_ARCHIVE)
+def extract_experiment():
+    run('unzip %s' % EXP_ARCHIVE)
 
-@task
-def test_installation(ctx):
-    with ctx.cd(EXP_DIRECTORY):
-        ctx.run('python3 ./runner.py --csv_file /tmp/test.csv --lib openblas --dgemm -s 64,64 -n 1 -r 1')
-        ctx.run('python3 ./multi_runner.py --nb_runs 10 --nb_calls 10 --size 100 -np 1 --csv_file /tmp/test.csv --lib naive --cpu_power=random --scheduler=random --thread_mapping=random --hyperthreading=random')
-        ctx.run('python3 ./multi_runner.py --nb_runs 3 --nb_calls 10 --size 100 -np 1 --csv_file /tmp/test.csv --lib naive --likwid CLOCK L3CACHE')
+def test_installation():
+    with cd(EXP_DIRECTORY):
+        run('python3 ./runner.py --csv_file /tmp/test.csv --lib openblas --dgemm -s 64,64 -n 1 -r 1')
+        run('python3 ./multi_runner.py --nb_runs 10 --nb_calls 10 --size 100 -np 1 --csv_file /tmp/test.csv --lib naive --cpu_power=random --scheduler=random --thread_mapping=random --hyperthreading=random')
+        run('python3 ./multi_runner.py --nb_runs 3 --nb_calls 10 --size 100 -np 1 --csv_file /tmp/test.csv --lib naive --likwid CLOCK L3CACHE')
 
-@task
-def all(ctx):
-    get_openblas_archive(ctx)
-    copy_archives(ctx)
-    install_apt_packages(ctx)
-    install_openblas(ctx)
-    install_pip_packages(ctx)
-    setup_os(ctx)
-    extract_experiment(ctx)
-    test_installation(ctx)
+def all():
+    get_openblas_archive()
+    copy_archives()
+    install_apt_packages()
+    install_openblas()
+    install_pip_packages()
+    setup_os()
+    extract_experiment()
+    test_installation()
